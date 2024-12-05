f14aeaa0be9cbadb5bad6d881d02ffbd7d811bc3
JeanEsg <160681687+JeanEsg@users.noreply.github.com>
2024-12-04 20:05:11 -0500

    V.1.7.5
    -Set the route to get all data
    -Fix the statusBar
    -Clean the recomendation of *** in the response

--- App.js
+++ App.js
@@ -7,8 +7,12 @@ import { SafeAreaProvider } from "react-native-safe-area-context";
 export default function App() {
   return (
     <SafeAreaProvider>
+      <StatusBar 
+        barStyle="dark-content" // Esto garantiza que los íconos sean visibles, incluso con un tema oscuro
+        hidden={false} // Esto asegura que la barra de estado no esté oculta
+        backgroundColor="transparent" // Hace que el fondo sea transparente 
+        />
       <View style={styles.container}>
-        <StatusBar style="dark" />
         <Main />
       </View>
     </SafeAreaProvider>
